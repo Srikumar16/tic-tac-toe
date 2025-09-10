a910d080b9778265631a11e59efcc48d41bc3e85
Srikumar16 <srikumar160307@gmail.com>
2025-09-10 16:06:55 +0530

    Add files via upload

---
+++ Tic tac toe.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+char board[3][3];
+void initializeBoard() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = ' ';
+        }
+    }
+}
+void printBoard() {
+    printf("\n");
+    for (int i = 0; i < 3; i++) {
+        printf(" %c | %c | %c ", board[i][0], board[i][1], board[i][2]);
+        if (i < 2) printf("\n---|---|---\n");
+    }
+    printf("\n");
+}
+int checkWin(char player) {
+    for (int i = 0; i < 3; i++) {
+        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
+            (board[0][i] == player && board[1][i] == player && board[2][i] == player))
+            return 1;
+    }
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player))
+        return 1;
+    return 0;
+}
+int isDraw() {
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i][j] == ' ')
+                return 0;
+    return 1;
+}
+
+int main() {
+    int row, col, turn = 0;
+    char player;
+
+    initializeBoard();
+
+    printf("Tic Tac Toe Game (Player X vs Player O)\n");
+    printBoard();
+
+    while (1) {
+        player = (turn % 2 == 0) ? 'X' : 'O';
+        printf("Player %c, enter row and column (0, 1, or 2): ", player);
+        scanf("%d %d", &row, &col);
+
+        if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != ' ') {
+            printf("Invalid move! Try again.\n");
+            continue;
+        }
+
+        board[row][col] = player;
+        printBoard();
+
+        if (checkWin(player)) {
+            printf("Player %c wins!\n", player);
+            break;
+        }
+        if (isDraw()) {
+            printf("It's a draw!\n");
+            break;
+        }
+
+        turn++;
+    }
+    return 0;
+}
